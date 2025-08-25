11c72444343c095c4034a2cbfa04a7310cb1e494
Foys17 <mhfoysal17@gmail.com>
2025-08-26 00:43:55 +0600

    Iitial project commit

---
+++ EdTech/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EdTech.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EdTech.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) 
+        {
+
+        }
+
+        public DbSet<Category>Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Web & App Development", DisplayOrder=1 },
+                new Category { Id = 2, Name = "Product Management & Design", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Business & Marketing", DisplayOrder = 3 }
+                );
+        }
+    }
+}
---
+++ EdTech/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace EdTech.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Category Name")]
+        public required string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100)]
+        
+        public int DisplayOrder { get; set; }
+    }
+}
